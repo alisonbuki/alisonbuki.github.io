ecf37a526679355bd9896bc334049891f066fb46
Alison Buki <alison.buki@gmail.com>
2018-06-16 23:43:13 -0400

    add initial babylon scene

--- buki-port/src/app/home/home.component.ts
+++ buki-port/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Game } from '.././classes/game';
+
+declare var babylonjs;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
+  title = 'my portfolio';
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    let game = new Game('render-canvas');
+    game.createScene();
+    game.animate();
   }
-
 }
